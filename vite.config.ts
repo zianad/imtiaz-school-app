05eec3e0672fa64ef0c005ce1036def030dcd57f
zianad <infinitydecosign@gmail.com>
2025-09-02 13:33:54 +0100

    Finalize project setup for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // By default, this plugin may only apply to .jsx and .tsx files.
+    // We explicitly include .ts and .js files to ensure components
+    // or files with hooks (like i18n.ts) are correctly processed.
+    include: '**/*.{js,jsx,ts,tsx}',
+  })],
 })
